5bc32001cabf308f8ad08438dafe705570835de4
ChanceLamberth <Lamberth.c@northeastern.edu>
2024-08-27 20:13:59 -0500

    Adding linting

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def testing(s:str):
+def testing2(s:int):
+    return s * 13
+
+
+def testing(s: str):
     print(s)
-
-
 
 
 if __name__ == "__main__":
     testing("hello World!")
+    testing2(1)
